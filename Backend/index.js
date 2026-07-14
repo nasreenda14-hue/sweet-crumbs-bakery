504a3c612f5b880076e3ef230eeba054f5c03b2d
Nasreen <nasreenda14@gmail.com>
2026-07-14 16:12:42 +0400

    feat: implement admin dashboard with product management and contact features - created admin dashboard layout - implemented add product page - added edit and delete functionality - built protected routes (admin only access) - developed dashboard home UI - integrated map, email, and WhatsApp contact features

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,23 @@
 import express from "express";
-import cors from 'cors'
-import dotenv from "dotenv"
+import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import productRoutes from './routes/productRoutes.js'
-import authRoutes from './routes/authRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
-connectDB()
+connectDB();
 
-app.use("/api/v1",productRoutes)
-app.use("/api/v1",authRoutes)
-
-
+app.use("/api/v1", productRoutes);
+app.use("/api/v1", authRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running at port ${process.env.PORT}`);
+  console.log(`Server running at port ${process.env.PORT}`);
 });
